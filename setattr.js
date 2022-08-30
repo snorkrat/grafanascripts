fe96a44fc5f0d5d42238a695f11dc6fb85f572c4
snorkrat <snorkrat@gmail.com>
2022-08-30 14:44:59 +0100

    Update setattr.js

--- setattr.js
+++ setattr.js
@@ -1,5 +1,11 @@
+//window.onload = function(){
+  // your code here
+  // first set panelid of panel in grafana
+  //document.querySelector('[data-panelid="63301"]').setAttribute("data-iframe-height","123");
+//};
+
 window.onload = function(){
   // your code here
   // first set panelid of panel in grafana
-  document.querySelector('[data-panelid="63301"]').setAttribute("data-iframe-height","123");
+  querySelector('[data-panelid="63301"]').dataset.iframe-height = "";
 };
