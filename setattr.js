0d780ef1c29898eb2908b91dff9a98f7ff2d5612
snorkrat <snorkrat@gmail.com>
2022-08-30 14:09:31 +0100

    Update setattr.js

--- setattr.js
+++ setattr.js
@@ -1,3 +1,28 @@
+// wait until DOM is fully loaded
+
+var observer = new MutationObserver(function(mutations) {
+    if ($("p").length) {
+        console.log("Exist, lets do something");
+        observer.disconnect(); 
+        //We can disconnect observer once the element exist if we dont want observe more changes in the DOM
+    }
+});
+
+// Start observing
+observer.observe(document.body, { //document.body is node target to observe
+    childList: true, //This is a must have for the observer with subtree
+    subtree: true //Set to true if changes must also be observed in descendants.
+});
+            
+$(document).ready(function() {
+    $("button").on("click", function() {
+        $("p").remove();
+        setTimeout(function() {
+            $("#newContent").append("<p>New element</p>");
+        }, 2000);
+    });
+});
+
 // first set panelid of panel in grafana
 
 document.querySelector('[data-panelid="63301"]').setAttribute("data-iframe-height","123");
