13e8c773bf00c18f29d9dbaf461039fc9c045518
snorkrat <snorkrat@gmail.com>
2022-08-30 14:55:36 +0100

    Update setattr.js

--- setattr.js
+++ setattr.js
@@ -1,6 +1,3 @@
-window.onload = function(){
-  // your code here
   // first set panelid of panel in grafana
   //document.querySelector('[data-panelid="63301"]').setAttribute("data-iframe-height","123");
   document.getElementsByClassName('react-grid-item').setAttribute("data-iframe-height","");
-};
