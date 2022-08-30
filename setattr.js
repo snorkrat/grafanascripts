cec90758aef1528ee972d09507a5876943c068eb
snorkrat <snorkrat@gmail.com>
2022-08-30 14:29:04 +0100

    Update setattr.js

--- setattr.js
+++ setattr.js
@@ -1,6 +1,5 @@
-// wait until DOM is fully loaded
-document.addEventListener("DOMContentLoaded", function(event){
+window.onload = function(){
   // your code here
   // first set panelid of panel in grafana
   document.querySelector('[data-panelid="63301"]').setAttribute("data-iframe-height","123");
-});
+};
