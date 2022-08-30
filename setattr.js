d78ed9253a15fe7520f600f8c27ef2f2af4a38b0
snorkrat <snorkrat@gmail.com>
2022-08-30 14:17:22 +0100

    Update setattr.js

--- setattr.js
+++ setattr.js
@@ -1,26 +1,21 @@
 // wait until DOM is fully loaded
 
-var observer = new MutationObserver(function(mutations) {
-    if ($("p").length) {
-        console.log("Exist, lets do something");
-        observer.disconnect(); 
-        //We can disconnect observer once the element exist if we dont want observe more changes in the DOM
+function handleSomeDiv(someDiv) { 
+    console.log("div was handled");
+}
+
+const observer = new MutationObserver(function (mutations, mutationInstance) {
+    const someDiv = document.querySelector('[data-panelid="63301"]');
+    if (someDiv) {
+        handleSomeDiv(someDiv);
+        mutationInstance.disconnect();
     }
 });
 
-// Start observing
-observer.observe(document.body, { //document.body is node target to observe
-    childList: true, //This is a must have for the observer with subtree
-    subtree: true //Set to true if changes must also be observed in descendants.
-});
-            
-$(document).ready(function() {
-    $("button").on("click", function() {
-        $("p").remove();
-        setTimeout(function() {
-            $("#newContent").append("<p>New element</p>");
-        }, 2000);
-    });
+
+observer.observe(document, {
+    childList: true,
+    subtree:   true
 });
 
 // first set panelid of panel in grafana
