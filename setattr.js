2907280eddf7c52b0b6bb420665209b7fe0ea4cf
snorkrat <snorkrat@gmail.com>
2022-08-30 15:31:29 +0100

    Update setattr.js

--- setattr.js
+++ setattr.js
@@ -1,3 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(event) {
       console.log("DOM fully loaded and parsed");
       document.getElementsByClassName('react-grid-item')[0].setAttribute("data-iframe-height","");
 });
