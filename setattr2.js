0dc2e5ca2df023bcef58684bcfa8907ecb2b0326
snorkrat <snorkrat@gmail.com>
2022-08-30 15:52:06 +0100

    Update setattr2.js

--- setattr2.js
+++ setattr2.js
@@ -3,7 +3,16 @@
       //document.getElementsByClassName('react-grid-item')[0].setAttribute("data-iframe-height","");
 //});
 
+//document.addEventListener("DOMContentLoaded", function(event) {
+	//console.log("DOM fully loaded and parsed");
+	//document.querySelector('[data-panelid="63301"]').setAttribute("data-iframe-height","123");
+//});
+
 document.addEventListener("DOMContentLoaded", function(event) {
 	console.log("DOM fully loaded and parsed");
-	document.querySelector('[data-panelid="63301"]').setAttribute("data-iframe-height","123");
+	var list, index;
+	list = document.querySelector('[data-panelid="63301"]');
+	for (index = 0; index < list.length; ++index) {
+		list[index].setAttribute("data-iframe-height", "123");
+	}
 });
