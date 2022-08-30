465b8c6d2d958bf972c8628cfb83cfe6b5c5fa32
snorkrat <snorkrat@gmail.com>
2022-08-30 15:46:08 +0100

    Create setattr2.js

---
+++ setattr2.js
@@ -0,0 +1,9 @@
+//document.addEventListener("DOMContentLoaded", function(event) {
+      //console.log("DOM fully loaded and parsed");
+      //document.getElementsByClassName('react-grid-item')[0].setAttribute("data-iframe-height","");
+//});
+
+document.addEventListener("DOMContentLoaded", function(event) {
+	console.log("DOM fully loaded and parsed");
+	document.querySelector('[data-panelid="63301"]').setAttribute("data-iframe-height","123");
+});
