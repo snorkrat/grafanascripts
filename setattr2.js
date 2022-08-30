7bae7d6b525694d62d282f15e70157eae84a7916
snorkrat <snorkrat@gmail.com>
2022-08-30 16:17:22 +0100

    Update setattr2.js

--- setattr2.js
+++ setattr2.js
@@ -1,4 +1,19 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-      console.log("DOM fully loaded and parsed");
-      document.getElementsByClassName('track-horizontal')[0].setAttribute("data-iframe-height","");
+/* Set configuration options and start the observer */
+ 
+// Select the entire DOM for observing:
+const target = document.querySelector('body');
+ 
+// Create a new observer instance:
+const observer = new MutationObserver(function() {
+    if (document.getElementsByClassName('react-grid-item')[0]) {
+        console.log("react-grid-item was just appended!");
+    }
 });
+ 
+// Set configuration object:
+const config = { childList: true };
+ 
+// Start the observer
+observer.observe(target, config);
+
+document.getElementsByClassName('track-horizontal')[0].setAttribute("data-iframe-height","");
