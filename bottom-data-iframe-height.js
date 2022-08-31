5f833c3c5d477028d207004ebd6107410564f6c3
snorkrat <snorkrat@gmail.com>
2022-08-31 08:03:02 +0100

    Delete bottom-data-iframe-height.js

--- bottom-data-iframe-height.js
+++
@@ -1,9 +0,0 @@
-var newDiv = document.createElement('div')
-newDiv.id = 'snorkdiv'
-newDiv.style.position = 'absolute'
-newDiv.style.bottom = "0px";
-newDiv.style.textAlign = "center";
-newDiv.style.width = "100%";
-newDiv.innerHTML = 'hello'
-//etc.
-document.body.appendChild(newDiv)
