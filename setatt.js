3f4f2a0a19d1245c6115dd480dcb0a371adf8472
snorkrat <snorkrat@gmail.com>
2022-08-31 10:26:42 +0100

    Update setatt.js

--- setatt.js
+++ setatt.js
@@ -1,30 +1,28 @@
 /* Set configuration options and start the observer */
- 
+
 // Select the entire DOM for observing:
 const target = document.querySelector('body');
- 
+
 // Create a new observer instance:
 const observer = new MutationObserver(function() {
-    if (document.getElementsByClassName('track-horizontal')) {
-        console.log("track-horizontal was just appended!");
-        var list, index;
-	    list = document.getElementsByClassName("react-grid-item");
-	    for (index = 0; index < list.length; ++index) {
-		list[index].setAttribute("data-iframe-height", "");
-	    }
-        console.log("Added data-iframe-height to all react-grid-items");
+	if (document.getElementsByClassName('track-horizontal')) {
+		console.log("track-horizontal was just appended!");
+		var list, index;
+		list = document.getElementsByClassName("react-grid-item");
+		for (index = 0; index < list.length; ++index) {
+			list[index].setAttribute("data-iframe-height", "");
+            console.log("Added data-iframe-height to all react-grid-items");
 
-	document.getElementsByClassName('css-1nm48fy').style.padding = "0px";
-        console.log("Added 0px padding to all css- classes");
-	    
-	var myPadding = document.querySelectorAll("[class^=css-]");
-	    myPadding.style.padding = "0px";
-        console.log("Added 0px padding to all css- classes");
-    }
+            document.querySelectorAll("[class^=css-]")[0].style.padding = "0px";
+		    console.log("Added 0px padding to all css- classes");
+		}
+	}
 });
- 
+
 // Set configuration object:
-const config = { childList: true };
- 
+const config = {
+	childList: true
+};
+
 // Start the observer
 observer.observe(target, config);
